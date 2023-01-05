d537a1232de43fc8d9873497ed14e9400c74ceaf
Pablo Viana <pablo71042001@furg.br>
2023-01-04 21:57:34 -0300

    created drop down selector

--- Front/src/components/Eventos/Eventos.jsx
+++ Front/src/components/Eventos/Eventos.jsx
@@ -1,11 +1,46 @@
 import styles from "./Eventos.module.css";
+import React, { useState } from "react";
 
 export function Eventos({ title }) {
+  const getInitialState = () => {
+    const value = "Null";
+    return value;
+  };
+
+  const [value, setValue] = useState(getInitialState);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
   return (
-    <main className={styles.mainMacroprocessos}>
-      <div className={styles.textMacro}>
+    <main className={styles.mainEventos}>
+      <div className={styles.textEventos}>
         <p>{title}</p>
       </div>
+      <div className={styles.seletorContainer}>
+        <p>{`Selecione a `}<b>{`FONTE`}</b></p>
+        <select value={value} onChange={handleChange} className={styles.seletor}>
+          <option value="Pessoas">Pessoas</option>
+          <option value="Processos">Processos</option>
+          <option value="Sistemas">Sistemas</option>
+          <option value="Infra">Infraestrutura Física</option>
+          <option value="EstruOrg">Estrutura Organizacional</option>
+          <option value="Tecnologia">Tecnologia</option>
+          <option value="EventosExt">Eventos Externos</option>
+        </select>
+        </div>
+
+        <div className={styles.seletorContainer}>
+        <p>{`Selecione o `}<b>{`TIPO`}</b></p>
+        <select value={value} onChange={handleChange} className={styles.seletorTipo}>
+          <option value="Estrategico">1-Risco Estrategico</option>
+          <option value="Operacional">2-Risco Operacional</option>
+          <option value="Conformidades">3-Risco de Conformidades</option>
+          <option value="Financeiro">4-Risco Financeiro/Orçamentário</option>
+          <option value="Imagem">5-Risco de Imagem</option>
+          <option value="Integridade">6-Risco de Integridade</option>
+        </select>
+        </div>
     </main>
   );
 }
