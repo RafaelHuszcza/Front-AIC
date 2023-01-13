f677915d47876e5b81768d8d525e488dc5c3ee7c
Pablo Viana <pablo71042001@furg.br>
2023-01-13 00:02:14 -0300

    solved css bug from tipos

--- Front/src/components/Eventos/Eventos.jsx
+++ Front/src/components/Eventos/Eventos.jsx
@@ -46,72 +46,84 @@ export function Eventos({ title }) {
             <div className={styles.row}>
               <div className={styles.leftContainer}>
                 
-                <p className={styles.seletorFirstHeader}>{`Evento`}</p>
-                <input className={styles.defaultInput} type="text" autoComplete="off" id="Evento" placeholder="Informe o nome do evento" />
+                <div className={styles.fakeCol}>
+                  <p>{`Evento`}</p>
+                  <input className={styles.defaultInput} type="text" autoComplete="off" id="Evento" placeholder="Informe o nome do evento" />
+                </div>
 
-                {causaList.map((singleCausa, index) => (
-                  <div key={index}>
-                    <p className={styles.seletorHeader}>{`Causas`}</p>
-                    <input className={styles.defaultInput} type="text" autoComplete="off" id="Causa" placeholder="Informe a causa do evento" required />
-                    {causaList.length - 1 === index && causaList.length < 3 && 
-                    (
-                      <span id="addCausa" onClick={handleClickCausa}><p className={styles.addCausaConsequencia}><img src={Add} className={styles.addMore} />Adicionar mais uma causa</p></span>
-                    )}
-                  </div>
-                ))}
 
-                {consequenciaList.map((singleConsequencia, index) => (
-                  <div key={index}>
-                    <p className={styles.seletorHeader}>{`Consequências`}</p>
-                    <input className={styles.defaultInput} type="text" autoComplete="off" id="Consequencia" placeholder="Informe as consequências o evento" required />
-                    {consequenciaList.length -1 === index && consequenciaList.length < 3 && 
-                    (
-                      <span id="addConsequencia" onClick={handleClickConsequencia}><p className={styles.addCausaConsequencia}><img src={Add} className={styles.addMore} />Adicionar mais uma consequência</p></span>
-                    )}
-                  </div>
-                ))}
+                <div className={styles.fakeCol}>
+                  <p>{`Atividade`}</p>
+                  <select  value={fontValue} onChange={e=>setFontes(e.target.value)} className={styles.seletor} required>
+                    <option className={styles.seletorLabel} disabled selected value="">Selecione a atividade relacionada</option>
+                  </select>
+                </div>
 
               </div>     
-
               
-              <div className={styles.leftContainer}>
+              <div className={styles.defaultContainer}>
                 
-                <p className={styles.seletorFirstHeader}>{`Atividade`}</p>
-                <select  value={fontValue} onChange={e=>setFontes(e.target.value)} className={styles.seletor} required>
-                  <option className={styles.seletorLabel} disabled selected value="">Selecione a atividade relacionada</option>
-                </select>
-                
-                {causaList.map((singleCausa, index) => (
-                  <div key={index}>
-                    <p className={styles.seletorHeader}>{`Fonte`}</p>
-                    <select value={fontValue} onChange={e=>setFontes(e.target.value)} className={styles.seletor} required>
-                      <option className={styles.seletorLabel} disabled selected value="">Selecione a fonte relacionada</option>
-                      <option value="Pessoas">Pessoas</option>
-                      <option value="Processos">Processos</option>
-                      <option value="Sistemas">Sistemas</option>
-                      <option value="Infraestrutura">Infraestrutura Física</option>
-                      <option value="Organizacional">Estrutura Organizacional</option>
-                      <option value="Tecnologia">Tecnologia</option>
-                      <option value="Eventos Externos">Eventos Externos</option>
-                    </select>
+                <div className={styles.fakeCol}>
+                  <p className={styles.seletorHeader}>{`Causas`}</p>
+                    {causaList.map((singleCausa, index) => (
+                      <div key={index}>
+                        <input className={styles.defaultInput} type="text" autoComplete="off" id="Causa" placeholder="Informe a causa do evento" required />
+                        {causaList.length - 1 === index && causaList.length < 3 && 
+                        (
+                          <span id="addCausa" onClick={handleClickCausa}><p className={styles.addCausaConsequencia}><img src={Add} className={styles.addMore} />Adicionar mais uma causa</p></span>
+                        )}
+                      </div>
+                    ))}
+                </div>
+                  
+                  <div className={styles.fakeCol}>
+                    <p className={styles.seletorHeader}>{`Fontes`}</p>
+                    {causaList.map((singleCausa, index) => (
+                      <div key={index}>
+                        <select value={fontValue} onChange={e=>setFontes(e.target.value)} className={styles.seletor} required>
+                          <option className={styles.seletorLabel} disabled selected value="">Selecione a fonte relacionada</option>
+                          <option value="Pessoas">Pessoas</option>
+                          <option value="Processos">Processos</option>
+                          <option value="Sistemas">Sistemas</option>
+                          <option value="Infraestrutura">Infraestrutura Física</option>
+                          <option value="Organizacional">Estrutura Organizacional</option>
+                          <option value="Tecnologia">Tecnologia</option>
+                          <option value="Eventos Externos">Eventos Externos</option>
+                        </select>
+                      </div>
+                    ))}
                   </div>
-                ))}
+              </div>
 
-                {consequenciaList.map((singleConsequencia, index) => (
-                  <div key={index}>
-                    <p className={styles.seletorHeaderTipo}>{`Tipo`}</p>
-                    <select value={tipoValue} onChange={e=>setTipos(e.target.value)} className={styles.seletor} required>
-                      <option value="" className={styles.seletorLabel} disabled selected>Selecione o tipo relacionado</option>
-                      <option value="Estrategico">1-Risco Estrategico</option>
-                      <option value="Operacional">2-Risco Operacional</option>
-                      <option value="Conformidades">3-Risco de Conformidades</option>
-                      <option value="Financeiro">4-Risco Financeiro/Orçamentário</option>
-                      <option value="Imagem">5-Risco de Imagem</option>
-                      <option value="Integridade">6-Risco de Integridade</option>
-                    </select>
-                  </div>
-                ))}
-
+              <div className={styles.defaultContainer}>
+                <div className={styles.fakeCol}>
+                  <p className={styles.seletorHeader}>{`Consequências`}</p>
+                  {consequenciaList.map((singleConsequencia, index) => (
+                    <div key={index}>
+                      <input className={styles.defaultInput} type="text" autoComplete="off" id="Consequencia" placeholder="Informe as consequências o evento" required />
+                      {consequenciaList.length -1 === index && consequenciaList.length < 3 && 
+                      (
+                        <span id="addConsequencia" onClick={handleClickConsequencia}><p className={styles.addCausaConsequencia}><img src={Add} className={styles.addMore} />Adicionar mais uma consequência</p></span>
+                      )}
+                    </div>
+                  ))}
+                </div>
+                <div className={styles.fakeCol}>
+                  <p className={styles.seletorHeader}>{`Tipos`}</p>
+                  {consequenciaList.map((singleConsequencia, index) => (
+                    <div key={index}>
+                      <select value={tipoValue} onChange={e=>setTipos(e.target.value)} className={styles.seletor} required>
+                        <option value="" className={styles.seletorLabel} disabled selected>Selecione o tipo relacionado</option>
+                        <option value="Estrategico">1-Risco Estrategico</option>
+                        <option value="Operacional">2-Risco Operacional</option>
+                        <option value="Conformidades">3-Risco de Conformidades</option>
+                        <option value="Financeiro">4-Risco Financeiro/Orçamentário</option>
+                        <option value="Imagem">5-Risco de Imagem</option>
+                        <option value="Integridade">6-Risco de Integridade</option>
+                      </select>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
 
